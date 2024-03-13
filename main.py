eb942249d59aa60ec4e2448e8e1140dd2c42bcc5
Georgi St. Lukanov <goshiii87@gmail.com>
2024-03-13 15:39:43 +0200

    .

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import FastAPI, Path
 
 app = FastAPI()
@@ -18,36 +20,37 @@ products = {
         {
             "name": "Milk",
             "price": 2.39,
-            "brand": "Nestle"
+            "brand": "Nestle",
         },
 
     2:
         {
-            "name": "Fanta",
+            "name": "Bread",
             "price": 1.39,
-            "brand": "Coca-Cola"
+            "brand": "HomeMade",
         },
 
     3:
         {
             "name": "Eggs",
             "price": 4.39,
-            "brand": "Home"
+            "brand": "Home",
         },
 }
 
 
 @app.get("/get-item/{item_id}")
-async def get_item(item_id: int = Path(description="The ID you want to see.", gt=0, lt=4)):
+def get_item(item_id: int = Path(description="The ID you want to see.", gt=0, lt=4)):
     return products[item_id]
 
 
 @app.get("/get-by-name")
-def get_item_by_name(name: str = None):
+def get_item_by_name(*, name: Optional[str] = None):
     for item_id in products:
         if products[item_id]["name"] == name:
             return products[item_id]
 
     return {"Data": "Not found!"}
 
-# http://127.0.0.1:8000/get-by-name?name=Milk
+# http://127.0.0.1:8000/get-by-name?name=Milk = {"name":"Milk","price":2.39,"brand":"Nestle"}
+# http://127.0.0.1:8000/get-by-name?name=Eggs = {"name":"Eggs","price":4.39,"brand":"Home"}
